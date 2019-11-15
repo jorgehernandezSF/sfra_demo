d8af2dc70fbc61cbbffa750630230451a068c651
Jorge J Hernandez <jorge.hernandez@salesforce.com>
2019-11-14 22:03:04 -0500

    Updating controller extend example. Removing client-side JS that never worked.

--- cartridges/sfra_demo/cartridge/client/default/js/productDetail.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var processInclude = require('base/util');
-
-$(document).ready(function () {
-    processInclude(require('./product/detail'));
-});
--- cartridges/sfra_demo/cartridge/controllers/Home.js
+++ cartridges/sfra_demo/cartridge/controllers/Home.js
@@ -4,26 +4,36 @@ var server = require('server');
 var cache = require('*/cartridge/scripts/middleware/cache');
 
 server.extend(module.superModule);
+'use strict';
 
-/****** Demo for simple append *******/
-//server.append('Show', function (req, res, next) {
-//    viewData = {
-//        demo1: 'sfra_demo cartridge append happening',
-//        demo2: 'this is the value overridden by the append'
-//    };
-//    res.setViewData(viewData);
-//    next();
-//});
+var server = require('server');
+//extend Home controller functionality using the extend method
+server.extend(module.superModule);
 
+//insert functionality before the Show route using server.prepend
+server.prepend('Show', function (req, res, next) {
+	var viewData = res.getViewData();
+	viewData.param1 = 'This is from prepend';
+	res.setViewData(viewData);
+	next();
+});
 
-/***** Demo for extending a middleware script: see cache.js on this cartridge *******/
+/* 
+ * Reuse the viewData from the prepend, and append new data + a query parameter value
+ * Also, use a custom middleware method: it extends the base cache.js script
+*/
 server.append('Show', cache.applyCustomCache, function (req, res, next) {
-    var viewData = {
-        demo1: 'this homepage is using custom cache middleware',
-        demo2: res.cachePeriod + ' ' + res.cachePeriodUnit
-    };
-    res.setViewData(viewData);
-    next();
+	var viewData = res.getViewData();
+	//declare param1 as a variable that equals 'General company details.'
+    var appendParam = 'This is from append';
+    var queryparam = req.querystring.param ? req.querystring.param : "no parameter was passed";
+
+	//Here grab whatever prepend added to viewData + the message here + the optional query string param
+	res.setViewData({
+		param1: viewData.param1 + ' AND ' + appendParam + ' AND querystring param = ' + queryparam,
+		param2: res.cachePeriod + ' ' + res.cachePeriodUnit
+	});
+	next();
 });
 
 module.exports = server.exports();
